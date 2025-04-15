226b07d89bdb2d06174a80d43cbfe0bd2ee784c6
JoaodeAlcantara <jgateixeira02@gmail.com>
2025-04-15 11:51:34 -0300

    adiciona funcionalidade de upload de foto de perfil e atualiza informações do usuário

--- js/userInfo.js
+++ js/userInfo.js
@@ -13,6 +13,17 @@ const saveButton = document.getElementById('saveButton');
 const deleteButton = document.getElementById('deleteButton');
 const confirmButton = document.getElementById('confirm');
 const modal = document.querySelector('.confirmModal');
+const userFoto = document.getElementById('userFoto');
+const foto = document.getElementById('profile-input');
+let file;
+
+foto.addEventListener('change', (e) => {
+    file = e.target.files?.[0];
+
+    if (file) {
+        userFoto.src = URL.createObjectURL(file);
+    }
+});
 
 let user;
 let currentUserId = null;
@@ -30,6 +41,10 @@ authUser().then(userData => {
         localizacaoInput.value = user.localizacao || '';
         generoSelect.value = user.genero || '';
         dataNascimentoInput.value = user.dt_nascimento || '';
+
+        if(user.foto){
+            userFoto.src = `http://localhost:3000/files/${user.foto}`;
+        }
     } else {
         currentUserName.textContent = 'Usuário não logado';
         currentUserEmail.textContent = '';
@@ -41,54 +56,69 @@ authUser().then(userData => {
 });
 
 saveButton.addEventListener('click', async () => {
+
     if (!currentUserId) {
         console.error('Usuário não logado ou ID não encontrado.');
         return;
     }
 
-        const updatedData = {
-            userName: user.userName,
-            email: user.email,
-            foto: user.foto,
-            descricao: userDescription.value,
-            num_telefone: telefoneInput.value,
-            genero: generoSelect.value,
-            localizacao: localizacaoInput.value,
-            dt_nascimento: dataNascimentoInput.value,
-            status: user.status,
-            cargo: user.cargo
-        };
+    const formData = new FormData();
 
-        const updateResponse = await fetch(`http://localhost:3000/usuarios/update/${currentUserId}`, {
-            method: 'PUT',
-            headers: {
-                'Content-Type': 'application/json',
-                'authorization': `Bearer ${localStorage.getItem('token')}`
-            },
-            body: JSON.stringify(updatedData),
-        });
+    if(foto) {
+        formData.append('file', file)
+    }
 
-        const result = await updateResponse.json();
+    formData.append('userName', user.userName);
+    formData.append('email', user.email);
+    formData.append('descricao', userDescription.value);
+    formData.append('num_telefone', telefoneInput.value);
+    formData.append('genero', generoSelect.value);
+    formData.append('localizacao', localizacaoInput.value);
+    formData.append('dt_nascimento', dataNascimentoInput.value);
 
-        if (updateResponse.ok) {
-            createToast('Informação','Informações atualizadas com sucesso!','padrao');
-        } else {
-            console.error('Erro ao atualizar informações:', result.message);
-            alert('Erro ao atualizar informações: ' + result.message);
-        }
+    if (
+        (formData.userName || '').trim() === (user.userName || '').trim() &&
+        (formData.email || '').trim() === (user.email || '').trim() &&
+        (formData.foto || '').trim() === (user.foto || '').trim() &&
+        (formData.descricao || '').trim() === (user.descricao || '').trim() &&
+        (formData.num_telefone || '').trim() === (user.num_telefone || '').trim() &&
+        (formData.genero || '').trim() === (user.genero || '').trim() &&
+        (formData.localizacao || '').trim() === (user.localizacao || '').trim() &&
+        (formData.dt_nascimento || '').trim() === (user.dt_nascimento || '').trim()
+    ) {
+        alert('Nenhuma alteração detectada');
+        return;
+    }
+
+    const updateResponse = await fetch(`http://localhost:3000/usuarios/update/${currentUserId}`, {
+        method: 'PUT',
+        headers: {
+            'authorization': `Bearer ${localStorage.getItem('token')}`
+        },
+        body: formData,
+    });
+
+    const result = await updateResponse.json();
+
+    if (updateResponse.ok) {
+        createToast('Informação','Informações atualizadas com sucesso!','padrao');
+    } else {
+        console.error('Erro ao atualizar informações:', result.message);
+        alert('Erro ao atualizar informações: ' + result.message);
+    }
 });
 
 deleteButton.addEventListener('click', () => {
-    if(modal.classList.contains('hide')){
+    if (modal.classList.contains('hide')) {
         modal.classList.remove('hide');
     } else {
         deleteButton.classList.add('hide');
     }
 })
 
-modal.addEventListener('click', () =>{modal.classList.add('hide')});
+modal.addEventListener('click', () => { modal.classList.add('hide') });
 
-document.querySelector('#cancel').addEventListener('click', () =>{
+document.querySelector('#cancel').addEventListener('click', () => {
     modal.classList.add('hide')
 })
 
